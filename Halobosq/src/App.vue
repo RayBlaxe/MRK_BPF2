340be0a472d45d37b2eb68a12e1c2c834e7088ce
Muhammad Raihan Khairullah <raihan21ti@mahasiswa.pcr.ac.id>
2023-05-18 17:45:23 +0700

    PErtemuan baru lagi

---
+++ Halobosq/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app" class="container mt-5">
+    <h1>IDShop</h1>
+    <product-list :product="product" :harga="harga" />
+  </div>
+</template>
+
+<script>
+import ProductList from "./components/productList.vue";
+// import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+// import Price from "./components/Price.vue";
+
+export default {
+  name: "App",
+  data: function () {
+    return {
+      harga: 50,
+      product: [],
+    };
+  },
+  components: {
+    ProductList,
+    // FontAwesomeIcon,
+    // Price,
+  },
+  mounted: function () {
+    fetch("http://hplussport.com/api/products/order/price")
+      .then((response) => response.json())
+      .then((data) => {
+        this.product = data;
+      });
+  },
+};
+</script>
---
+++ Halobosq/src/components/Price.vue
@@ -0,0 +1,25 @@
+<template>
+  <span>
+    {{
+      this.prefix + Number.parseFloat(this.value).toFixed(this.precission)
+    }}</span
+  >
+</template>
+<script>
+export default {
+  name: "PriceItem",
+  props: {
+    value: {
+      type: Number,
+    },
+    prefix: {
+      type: String,
+      default: "$",
+    },
+    precission: {
+      type: Number,
+      default: 2,
+    },
+  },
+};
+</script>
